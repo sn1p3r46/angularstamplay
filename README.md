e80a129c362dc8629a24f5b925f8ecf2fa610905
sn1p3r46 <kingokongo46@gmail.com>
2016-01-13 06:06:16 +0100

    1st commit

---
+++ README.md
@@ -0,0 +1,3 @@
+http://apidaysberlin2015.apistrat.com/wp-content/uploads/2015/04/Stamplay_logo_squared.png
+
+
---
+++ app/services/user.js
@@ -0,0 +1,39 @@
+angular
+  .module('UserService', [])
+  .factory('User', ['$q', '$stamplay', UserService]);
+
+function UserService($q, $stamplay) {
+
+  return {
+    getCurrent: getCurrent,
+    logout: logout
+  };
+
+  // get the current logged in user
+  function getCurrent() {
+    var deferred = $q.defer();
+
+    // instantiate the user model from the sdk
+    var userModel = $stamplay.User().Model;
+
+    userModel.currentUser()
+      .then(function() {
+        deferred.resolve(userModel);
+      })
+      .catch(function(err) {
+        deferred.reject(err);
+      });
+
+    return deferred.promise;
+  }
+
+  // logout function to clear the token from 
+  function logout() {
+
+    // instantiate the user model from the sdk
+    var userModel = $stamplay.User().Model;
+
+    userModel.logout();
+  }
+
+}
